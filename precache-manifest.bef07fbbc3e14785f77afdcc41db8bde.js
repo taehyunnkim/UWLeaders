8cdec0d2424fd4510b5c8c6c08a6dca0fc591c34
Eric Kim <kim2000@uw.edu>
2020-04-21 21:30:29 +0900

    Updates

---
+++ precache-manifest.bef07fbbc3e14785f77afdcc41db8bde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6f34788595bd163b35f0026bd246c4",
+    "url": "/UWLeaders/index.html"
+  },
+  {
+    "revision": "e3d1948d07e668115377",
+    "url": "/UWLeaders/static/css/main.744009b2.chunk.css"
+  },
+  {
+    "revision": "128f435e550b9fe823c0",
+    "url": "/UWLeaders/static/js/2.602a571d.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/UWLeaders/static/js/2.602a571d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d1948d07e668115377",
+    "url": "/UWLeaders/static/js/main.217a6fee.chunk.js"
+  },
+  {
+    "revision": "b77bfbe4cd745660b743",
+    "url": "/UWLeaders/static/js/runtime-main.ede5627c.js"
+  },
+  {
+    "revision": "cad79d7b0e005e0fcb576cb9dc65439b",
+    "url": "/UWLeaders/static/media/uwleaders.cad79d7b.svg"
+  }
+]);
