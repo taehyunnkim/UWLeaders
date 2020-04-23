cb7ebf2da0bf891601834d7e330eb20c62feed8c
Eric Kim <kim2000@uw.edu>
2020-04-23 23:17:00 +0900

    Updates

---
+++ precache-manifest.032f038bc9343154f857be92a189752c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b74125f016bc855c14be828b28925e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8497968752085ad1b891",
+    "url": "/static/css/main.95a81a23.chunk.css"
+  },
+  {
+    "revision": "90f12035704df7f64af5",
+    "url": "/static/js/2.42c7cae5.chunk.js"
+  },
+  {
+    "revision": "2b166f0dc6b3f9bdcafac7b7f10c4940",
+    "url": "/static/js/2.42c7cae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8497968752085ad1b891",
+    "url": "/static/js/main.89b13e71.chunk.js"
+  },
+  {
+    "revision": "b29143fca7a5921646b9",
+    "url": "/static/js/runtime-main.f3368db2.js"
+  },
+  {
+    "revision": "d2581bd370960acc7be5ae2eced1b44a",
+    "url": "/static/media/logo-bg.d2581bd3.png"
+  },
+  {
+    "revision": "b3e164c4ba48cbaf1374e59cbcb9fcaa",
+    "url": "/static/media/logo-fg.b3e164c4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/UWLeaders/precache-manifest.dcf0b4f195aabd4719cd809bb9334c3d.js"
+  "/precache-manifest.032f038bc9343154f857be92a189752c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/UWLeaders/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
