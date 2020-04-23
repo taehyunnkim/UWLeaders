117f7dbf5b6b78e7919253def9b2ebca4c343526
Eric Kim <kim2000@uw.edu>
2020-04-23 20:52:39 +0900

    Add animation to homepage

--- src/components/dashboards/HomeDashboard.js
+++ src/components/dashboards/HomeDashboard.js
@@ -1,27 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { gsap, Power4 } from 'gsap';
 import { UserContext } from '../session/session';
-import logo from '../../images/uwl-logo.png';
+import logoBg from '../../images/logo-bg.png';
+import logoFg from '../../images/logo-fg.png';
 import asuw from '../../images/asuw.png';
 
-export default function HomeDashboard({handleSignout}) {
+class HomeDashboard extends Component {
 
-  return (
-    <div className='home'>
-      <div>
-        <h1>Leaders Are <strong>Made</strong></h1>
-        <h1>Not Born.</h1>
-        <UserContext.Consumer>
-          {auth => 
-            auth
-              ? <button className='btn-large homeBtn' onClick={handleSignout}>Sign Out</button>
-              : <button className='btn-large homeBtn' onClick={handleSignout}>JOIN US</button>
-              }
-        </UserContext.Consumer>
+  componentDidMount() {
+    gsap.to(this.content, 0.5, {
+      opacity: 1,
+      ease: Power4.easeIn
+    });
+    gsap.from(this.content, 1, {
+      y: 30,
+      ease: Power4
+    });
+    document.addEventListener('mousemove', this.parallax);
+  }
+
+  parallax = (e) => {
+    const width = document.body.getBoundingClientRect().width;
+    const height = document.body.getBoundingClientRect().height;
+
+    gsap.to(this.logoFg, 1, {
+        x: (e.pageX - width / 2) / width * 5,
+        y: (e.pageY - height / 2) / height * 15
+    });
+
+    gsap.to(this.logoBg, 1, {
+      x: (e.pageX - width / 2) / width * -5,
+      y: (e.pageY - height / 2) / height * -15
+  });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousemove', this.parallax);
+  }
+
+  render(){
+    return (
+      <div className='home'>
+        <div ref={div => (this.content = div)}>
+          <h1>Leaders Are <strong>Made</strong></h1>
+          <h1>Not Born.</h1>
+          <UserContext.Consumer>
+            {auth => 
+              auth
+                ? <button className='btn-large homeBtn' onClick={this.props.handleSignout}>Sign Out</button>
+                : <button className='btn-large homeBtn' onClick={this.props.handleSignout}>JOIN US</button>
+                }
+          </UserContext.Consumer>
+        </div>
+        <div ref={div => (this.img = div)} className='logo'>
+          <img ref={img => (this.logoFg = img)} src={logoFg} alt='uwleaders' />
+          <img ref={img => (this.logoBg = img)} src={logoBg} alt='uwleaders' />
+        </div>
+        <img className='asuw' src={asuw} alt='asuw logo' />
       </div>
-      <div className='logo'>
-        <img src={logo} alt='uwleaders logo' width='200' height='200' />
-      </div>
-      <img className='asuw' src={asuw} alt='asuw logo' />
-    </div>
-  )
+    )
+  }
 }
+
+export default HomeDashboard;
